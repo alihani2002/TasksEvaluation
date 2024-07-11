775b90427c89fcc487c094eaf7708b8024e0ba44
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-07-11 14:11:39 +0200

    UpdateSender

---
+++ TasksEvaluation.Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using TasksEvaluation.Infrastructure.Helpers;
+
+
+namespace TasksEvaluation.Infrastructure.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly MailSettings mailSettings;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public EmailSender(IOptions<MailSettings> _mailSettings, IWebHostEnvironment _webHostEnvironment)
+        {
+            mailSettings = _mailSettings.Value;
+            webHostEnvironment = _webHostEnvironment;
+        }
+        public async Task SendEmailAsync(string email, string subject, string content)
+        {
+            if (string.IsNullOrEmpty(email))
+                return;
+
+            var message = new MailMessage();
+            message.From = new MailAddress(mailSettings.Email!, mailSettings.DisplayName);
+            message.Subject = subject;
+            message.To.Add(email);
+            message.Body = $"<html><body>{content}</body></html>";
+            message.IsBodyHtml = true;
+
+
+            var smtpClient = new SmtpClient(mailSettings.Host)
+            {
+                Port = mailSettings.Port,
+                Credentials = new NetworkCredential(mailSettings.Email, mailSettings.Password),
+                EnableSsl = true,
+            };
+
+            await smtpClient.SendMailAsync(message);
+            smtpClient.Dispose();
+        }
+    }
+}
