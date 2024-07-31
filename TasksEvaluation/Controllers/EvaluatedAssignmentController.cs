4c091424c6a204b1ae6449acd24cd822bfc9f5bc
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-07-31 16:21:17 +0200

    AddEvaluated

---
+++ TasksEvaluation/Controllers/EvaluatedAssignmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TasksEvaluation.Controllers
+{
+    public class EvaluatedAssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
