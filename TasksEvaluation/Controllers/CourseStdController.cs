5c4147fd68b844005fa35ffab4097fe20237e653
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-07-31 14:03:23 +0200

    UpdateCouse

---
+++ TasksEvaluation/Controllers/CourseStdController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TasksEvaluation.Core.Interfaces.IServices;
+
+namespace TasksEvaluation.Controllers
+{
+    public class CourseStdController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseStdController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        // GET: Course
+        public async Task<IActionResult> Index()
+        {
+            var courseDTOs = await _courseService.GetCourses();
+            return View(courseDTOs);
+        }
+
+        // GET: Course/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var courseDTO = await _courseService.GetCourse(id);
+            if (courseDTO == null)
+            {
+                return NotFound();
+            }
+            return View(courseDTO);
+        }
+    }
+}
