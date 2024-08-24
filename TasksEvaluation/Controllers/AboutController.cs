285b9b6f9c8daad821e662ff1bbc9b9beea0013f
alyhani2002 <104443638+alihani2002@users.noreply.github.com>
2024-08-24 15:07:54 +0200

    Add about page

---
+++ TasksEvaluation/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TasksEvaluation.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
